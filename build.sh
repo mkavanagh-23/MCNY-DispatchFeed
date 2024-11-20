1d12c24d55d854373ea4082a07cf40e4ce93877f
Matthew Kavanagh <matthewfkavanagh@icloud.com>
2024-11-20 14:23:25 +0000

    Update linux G++ build script

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 g++ -o rocCAD main.cpp -lcurl
+if [[ $? -eq 0 ]]; then
+  echo -e "\033[32mCompilation Success! ./rocCAD to run\033[0m"
+else
+  echo -e "\033[31mCompilation failed. Check logs\033[0m"
+fi
